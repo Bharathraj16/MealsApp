fb9445fd701c21b08a71ba3f3e294325243fb95f
bharathraj <bharathraj16903@gmail.com>
2024-03-22 13:32:28 +0530

    Fetch datas in API

---
+++ app/src/main/java/com/example/mealsapp/Category.kt
@@ -0,0 +1,8 @@
+package com.example.mealsapp
+
+data class Category(val idCategory:String, val strCategory:String,val strCategoryThumb:String,val strCategoryDescription:String ) {
+}
+
+data class CategoriesResponse(val categorie:List<Category>){
+
+}
