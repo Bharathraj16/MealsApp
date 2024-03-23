ec5cf775ed63d754252c7e1027effccd21309c15
bharathraj <bharathraj16903@gmail.com>
2024-03-23 12:58:23 +0530

    MainActivity code update

--- app/src/main/java/com/example/mealsapp/MainActivity.kt
+++ app/src/main/java/com/example/mealsapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mealsapp.ui.theme.MealsAppTheme
+import eu.tutorials.myrecipeapp.RecipeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RecipeScreen()
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MealsAppTheme {
-        Greeting("Android")
     }
 }
